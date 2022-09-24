d753e70ed850890d05ed18437d80af2a5a151dae
mfkucuk <mfkucuk555@gmail.com>
2022-09-24 21:12:49 +0300

    Update AudioManager.cs

--- AudioManager-Unity/Assets/AudioManager.cs
+++ AudioManager-Unity/Assets/AudioManager.cs
@@ -7,21 +7,68 @@ public class AudioManager : MonoBehaviour
     [SerializeField]
     private Sound[] _soundList;
 
+    private AudioSource _audioSource;
+
     private void Start()
     {
-        foreach (Sound sound in _soundList)
-        {
-            
-        }
+        _audioSource = GetComponent<AudioSource>();
+        PlaySoundEffect("testSound");
     }
 
-    public bool PlayAudio(string soundName)
+    /**
+     * Important info: this function should only be used when playing sound effects.
+     * (such as jumping, hurting, dealing damage and so on.)
+     * @param audioName: name of the sound effect to be played.
+     */
+    public bool PlaySoundEffect(string audioName)
     {
         foreach (Sound sound in _soundList)
         {
-            if (soundName.Equals(sound.audioName))
+            if (audioName.Equals(sound.audioName))
             {
-                
+                // Play the audio source
+                _audioSource.PlayOneShot(sound.audioClip, sound.volume);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Important info: this function should only be used when playing music.
+     * @param musicName: name of the music to be played.
+     */
+    public bool PlayMusic(string musicName)
+    {
+        foreach (Sound sound in _soundList)
+        {
+            if (musicName.Equals(sound.audioName))
+            {
+                // Assign the necessary information from the scriptable object
+                _audioSource.clip = sound.audioClip;
+                _audioSource.volume = sound.volume;
+                _audioSource.pitch = sound.pitch;
+                _audioSource.loop = sound.isLooping;
+                _audioSource.playOnAwake = sound.isPlayedOnAwake;
+
+                // Play the audio source
+                _audioSource.Play();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool StopMusic(string audioName)
+    {
+        foreach (Sound sound in _soundList)
+        {
+            if (audioName.Equals(sound.audioName))
+            {
+                // Stop the audio source
+                _audioSource.Stop();
                 return true;
             }
         }
